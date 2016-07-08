87daf838beea8f88e127e78cb8c223f2bbf59819
lorenzo perone <lorenzo.perone@gmail.com>
2016-07-08 10:39:33 +0200

    conversione dei file dbf attributi degli shape file in file cdv attraverso uno script che usa csvkit

---
+++ convert_dbf_to_csv.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Converte i file dbf contenenti gli attributi degli shape file in file csv
+
+in2csv ./dati_dbtr_V_NCV_GPT_Bologna/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
+in2csv ./dati_dbtr_V_NCV_GPT_Bologna/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
+
+in2csv ./dati_dbtr_V_NCV_GPT_Ferrara/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
+in2csv ./dati_dbtr_V_NCV_GPT_Ferrara/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
+
+in2csv ./dati_dbtr_V_NCV_GPT_Forli-Cesena/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
+in2csv ./dati_dbtr_V_NCV_GPT_Forli-Cesena/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
+
+in2csv ./dati_dbtr_V_NCV_GPT_Modena/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
+in2csv ./dati_dbtr_V_NCV_GPT_Modena/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
+
+in2csv ./dati_dbtr_V_NCV_GPT_Parma/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
+in2csv ./dati_dbtr_V_NCV_GPT_Parma/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
+
+in2csv ./dati_dbtr_V_NCV_GPT_Piacenza/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
+in2csv ./dati_dbtr_V_NCV_GPT_Piacenza/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
+
+in2csv ./dati_dbtr_V_NCV_GPT_Ravenna/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
+in2csv ./dati_dbtr_V_NCV_GPT_Ravenna/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
+
+in2csv ./dati_dbtr_V_NCV_GPT_ReggioEmilia/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
+in2csv ./dati_dbtr_V_NCV_GPT_ReggioEmilia/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
+
+in2csv ./dati_dbtr_V_NCV_GPT_Rimini/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
+in2csv ./dati_dbtr_V_NCV_GPT_Rimini/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
