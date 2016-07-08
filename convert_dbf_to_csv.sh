319a64971b64b76f0c71fdad44015f27936948ff
lorenzo perone <lorenzo.perone@gmail.com>
2016-07-08 11:06:14 +0200

    corretti errori e inserito parametro per conversione da Latin1 a UTF-8

--- convert_dbf_to_csv.sh
+++ convert_dbf_to_csv.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 #Converte i file dbf contenenti gli attributi degli shape file in file csv
 
-in2csv ./dati_dbtr_V_NCV_GPT_Bologna/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
-in2csv ./dati_dbtr_V_NCV_GPT_Bologna/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_Bologna/V_NCV_GPT.dbf > ./V_NCV_GPT_Bologna.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_Bologna/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_Bologna.csv
 
-in2csv ./dati_dbtr_V_NCV_GPT_Ferrara/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
-in2csv ./dati_dbtr_V_NCV_GPT_Ferrara/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_Ferrara/V_NCV_GPT.dbf > ./V_NCV_GPT_Ferrara.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_Ferrara/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_Ferrara.csv
 
-in2csv ./dati_dbtr_V_NCV_GPT_Forli-Cesena/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
-in2csv ./dati_dbtr_V_NCV_GPT_Forli-Cesena/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_Forli-Cesena/V_NCV_GPT.dbf > ./V_NCV_GPT_Forli-Cesena.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_Forli-Cesena/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_Forli-Cesena.csv
 
-in2csv ./dati_dbtr_V_NCV_GPT_Modena/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
-in2csv ./dati_dbtr_V_NCV_GPT_Modena/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_Modena/V_NCV_GPT.dbf > ./V_NCV_GPT_Modena.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_Modena/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_Modena.csv
 
-in2csv ./dati_dbtr_V_NCV_GPT_Parma/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
-in2csv ./dati_dbtr_V_NCV_GPT_Parma/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_Parma/V_NCV_GPT.dbf > ./V_NCV_GPT_Parma.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_Parma/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_Parma.csv
 
-in2csv ./dati_dbtr_V_NCV_GPT_Piacenza/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
-in2csv ./dati_dbtr_V_NCV_GPT_Piacenza/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_Piacenza/V_NCV_GPT.dbf > ./V_NCV_GPT_Piacenza.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_Piacenza/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_Piacenza.csv
 
-in2csv ./dati_dbtr_V_NCV_GPT_Ravenna/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
-in2csv ./dati_dbtr_V_NCV_GPT_Ravenna/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_Ravenna/V_NCV_GPT.dbf > ./V_NCV_GPT_Ravenna.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_Ravenna/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_Ravenna.csv
 
-in2csv ./dati_dbtr_V_NCV_GPT_ReggioEmilia/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
-in2csv ./dati_dbtr_V_NCV_GPT_ReggioEmilia/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_ReggioEmilia/V_NCV_GPT.dbf > ./V_NCV_GPT_ReggioEmilia.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_ReggioEmilia/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_ReggioEmilia.csv
 
-in2csv ./dati_dbtr_V_NCV_GPT_Rimini/V_NCV_GPT.dbf > ./V_NCV_GPT_PIACENZA.csv
-in2csv ./dati_dbtr_V_NCV_GPT_Rimini/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_PIACENZA.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_Rimini/V_NCV_GPT.dbf > ./V_NCV_GPT_Rimini.csv
+in2csv -e iso-8859-1 ./dati_dbtr_V_NCV_GPT_Rimini/V_NCV_PROTEZ_GPT.dbf > ./V_NCV_GPT_PROTEZ_Rimini.csv
